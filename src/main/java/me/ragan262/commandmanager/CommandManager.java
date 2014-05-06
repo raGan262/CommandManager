926a89bcabed52f773c784187ee73c1f65793658
raGan <ragan.ragan@gmail.com>
2014-05-06 04:12:57 +0200

    made CommandManager class final

--- src/main/java/me/ragan262/commandmanager/CommandManager.java
+++ src/main/java/me/ragan262/commandmanager/CommandManager.java
@@ -30,7 +30,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class CommandManager {
+public final class CommandManager {
 	
 	Logger logger = null;
 	
